0715f6fbf4e2e6889404262a6e82a7e7f1448f27
vagrant <vagrant@precise32.(none)>
2014-02-14 23:36:30 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,14 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, displayInfo);
+}
+
+function displayInfo(result) {
+	console.log(result);
+	var projectHTML = "<img src=\"" + result['image'] + "\" class=\"detailsImage\"><p>" + result['date'] + "</p>" + result['summary'];
+	$("#project" + result['id'] + " .details").html(projectHTML);
 }
 
 /*
@@ -35,4 +43,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", displayInfo).done(function(result) {
+		var colors = result.colors.hex;
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
+
+
